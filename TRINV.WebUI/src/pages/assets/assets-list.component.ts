88d25e4f33f1d320ef78229e24832bd92ddff9e9
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-02-16 19:35:17 +0200

    IN-0 Assets design

--- TRINV.WebUI/src/pages/assets/assets-list.component.ts
+++ TRINV.WebUI/src/pages/assets/assets-list.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Animation,
+  CategoryScale,
+  Chart,
+  ChartOptions,
+  Filler,
+  LineController,
+  LineElement,
+  LinearScale,
+  PointElement,
+  RadialLinearScale,
+  Ticks,
+  Tooltip,
+} from 'chart.js';
 import { Observable, of } from 'rxjs';
 
 export interface ShipData {
@@ -22,6 +36,66 @@ export const exampleShips: ShipData[] = [
     totalPrice: 15003,
   },
   {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  },{
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  },{
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
+    assetId: 'tth',
+    name: 'Tether',
+    totalQuantity: 495,
+    totalPrice: 500,
+  }, {
     assetId: 'tth',
     name: 'Tether',
     totalQuantity: 495,
@@ -50,12 +124,192 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class AssetsListComponent {
   displayedRows$!: Observable<ShipData[]>;
+  value = 'Clear me';
 
   displayedColumns: string[] = ['dateAdded', 'quantity', 'price', 'actions'];
+  public chartData: any | null = null;
 
   dataSource = ELEMENT_DATA;
+  public chart: any;
+
+  constructor() {
+    Chart.register(
+      LinearScale,
+      LineController,
+      CategoryScale,
+      PointElement,
+      LineElement,
+      Tooltip,
+      Filler,
+      
+    );
+  }
+
   ngOnInit() {
     const rows$ = of(exampleShips);
     this.displayedRows$ = rows$;
+
+    this.createChart();
   }
+
+public testChart(){
+ let  options: { label: string, value: string }[] = [
+    {
+      label: 'Today',
+      value: 'today',
+    },
+    {
+      label: 'Last 7 Days',
+      value: '7days',
+    },
+    {
+      label: 'Last 30 Days',
+      value: '30days',
+    },
+    {
+      label: 'Last 6 Months',
+      value: '6months',
+    },
+    {
+      label: 'This Year',
+      value: 'year',
+    },
+  ];
+
+this.chart = new Chart("MyChart", {
+  type: "line",
+  data: {
+    labels: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],    datasets: [
+      {
+        label: "Income",
+        backgroundColor: "rgba(102, 126, 234, 0.25)",
+        borderColor: "rgba(102, 126, 234, 1)",
+        pointBackgroundColor: "rgba(102, 126, 234, 1)",
+        data: [13, 16, 21, 28, 32, 34, 32, 31, 30, 26, 20,100],
+      },
+      {
+        label: "Expenses",
+        backgroundColor: "rgba(237, 100, 166, 0.25)",
+        borderColor: "rgba(237, 100, 166, 1)",
+        pointBackgroundColor: "rgba(237, 100, 166, 1)",
+        data: [13, 16, 21, 28, 32, 34, 32, 31, 30, 26, 20,100],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        grid: {
+          display: false
+        },
+        ticks: {
+          callback(tickValue, index, ticks) {
+            let value  =tickValue as number;
+            return value > 1000 ? ((value < 1000000) ? value / 1000 + 'K' : value / 1000000 + 'M') : value;
+
+          }
+        }
+      }
+    }
+  }
+});
+}
+
+  createChart() {
+    this.chart = new Chart('MyChart', {
+      type: 'line',
+      data: {
+        labels: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec',
+        ],
+        datasets: [
+          {
+            label: 'Temperature(C) in Lahore',
+            data: [13, 16, 21, 28, 32, 34, 32, 31, 30, 26, 20,100],
+            backgroundColor: "rgba(102, 126, 234, 0.25)",
+            borderColor: "rgba(102, 126, 234, 1)",
+            pointBackgroundColor: "rgba(102, 126, 234, 1)",
+            borderWidth: 2,
+            fill:"origin"
+          },
+          {
+            label: 'Cloud Coverage(%) in Lahore',
+            data: [26, 29, 31, 24, 10, 3, 14, 16, 4, 5, 23,50],
+            backgroundColor: "rgba(237, 100, 166, 0.25)",
+            borderColor: "rgba(237, 100, 166, 1)",
+            pointBackgroundColor: "rgba(237, 100, 166, 1)",
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        responsive:true,
+        aspectRatio: 2.5,
+        scales: {
+          y: {
+            beginAtZero: true,
+            grid: {
+              color: 'rgba(225, 255, 255, 0.2)',
+              lineWidth:2,
+            },
+            border: {
+              dash: [1,10],
+          },
+            ticks: { color: 'rgb(157,163,175)' },
+          },
+          x: {
+            grid: {
+              color: 'rgba(225, 255, 255, 0.02)',
+
+            },
+            ticks: { color: 'rgb(157,163,175)' },
+          },
+        },
+        plugins: {
+          tooltip: {
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            bodyFont: {
+              size: 14,
+            },
+            titleFont: {
+              size: 16,
+              weight: 'bold',
+            },
+          },
+          legend: {
+            labels: {
+              font: {
+                size: 14,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
 }
--- TRINV.WebUI/src/pages/assets/assets-list.module.ts
+++ TRINV.WebUI/src/pages/assets/assets-list.module.ts
@@ -7,10 +7,13 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [AssetsListComponent],
-  imports: [MatExpansionModule,MatTableModule,MatIconModule,MatMenuModule,MatListModule,MatSelectModule,SharedModule],
+  imports: [MatPaginatorModule,MatTooltipModule,MatCardModule,MatExpansionModule,MatTableModule,MatIconModule,MatMenuModule,MatListModule,MatSelectModule,SharedModule],
   exports: [],
   providers: [],
 })
