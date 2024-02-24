fe53ac415da69e32a9c547f66b1262a7f9cdbd4f
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-02-24 21:32:33 +0200

    in-0 redesign generic table

--- TRINV.WebUI/src/features/dashboard/components/dashboard-generic-table/dashboard-generic-table.module.ts
+++ TRINV.WebUI/src/features/dashboard/components/dashboard-generic-table/dashboard-generic-table.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../../../../shared/shared.module";
 import { DashboardGenericTableDialogComponent } from "./page/dashboard-generic-table.component";
 import { MatDialogClose, MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
     declarations: [DashboardGenericTableDialogComponent],
     imports: [
       SharedModule,
       MatDialogModule,
-      MatDialogClose
+      MatDialogClose,
+      MatIconModule,
     ],
     exports: [DashboardGenericTableDialogComponent],
     providers: [],
