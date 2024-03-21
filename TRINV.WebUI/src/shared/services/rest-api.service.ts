903c588500e1be79e9ddac9924a5a48f4bc6ea79
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2024-03-21 23:11:02 +0200

    cleaning rest api service

--- TRINV.WebUI/src/shared/services/rest-api.service.ts
+++ TRINV.WebUI/src/shared/services/rest-api.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from "@angular/core";
 import { Observable, map } from "rxjs";
 
 export const URL_INVEST_TRACKER: string = "https://localhost:7270"
-// export const URL_CLIENT: string = "http://localhost:7270";
 
 @Injectable({
     providedIn: 'root',
@@ -16,7 +15,6 @@ export const URL_INVEST_TRACKER: string = "https://localhost:7270"
     }
   
     public get<T>(path: string, options?: object): Observable<T> {
-      console.log('From Rest api service', `${this.baseUrl}${path}`);
       return this.http
         .get<T>(`${this.baseUrl}${path}`, options)
         .pipe(map((result: any) => result as T));
@@ -27,7 +25,6 @@ export const URL_INVEST_TRACKER: string = "https://localhost:7270"
       body: object | string,
       options?: object
     ): Observable<T | null> {
-      console.log(`test`, `${this.baseUrl}${path}`);
       return this.http
         .post<T>(`${this.baseUrl}${path}`, body, options)
         .pipe(map((res: any) => res as T));
